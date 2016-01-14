04b58303c35b93f73adcc34ecc6c3131197b6fe6
Enrico Benini <benkio89@gmail.com>
2016-01-14 17:02:37 +0100

    previous commit

--- Server/app/controllers/Application.java
+++ Server/app/controllers/Application.java
@@ -15,22 +15,6 @@ public class Application extends Controller {
 	return ok(redirect.render());
     }
 
-    public Result newRange(){
-	//Make the form look pretty
-	Form<Range> rangeForm = Form.form(Range.class);
-	return ok(newrange.render(rangeForm));
-    }
-
-    public Result submitNewRange(){
-	//TODO: get data and store it.
-	return ok();
-    }
-
-    public Result ranges(){
-	//TODO: fetch data from database and send it back
-	return ok(index.render("TODO GET RANGES"));
-    }
-
     public Result status(){
 	//TODO: add API for send data back (AJAX)
 	return ok(status.render());
