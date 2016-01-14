1a480501861b0c2aceb4bf26edafb4a36fc88f34
Enrico Benini <benkio89@gmail.com>
2016-01-14 13:58:35 +0100

    add another route for retrive range lists

--- Server/app/controllers/Application.java
+++ Server/app/controllers/Application.java
@@ -7,22 +7,33 @@ import play.mvc.*;
 import views.html.*;
 import models.Range;
 
+//TODO: move all in a different controllers
+
 public class Application extends Controller {
 
     public Result analysis(){
+	// TODO: what?
 	return ok(index.render("TODO ANALYSIS"));
     }
 
     public Result newRange(){
+	//Make the form look pretty
 	Form<Range> rangeForm = Form.form(Range.class);
 	return ok(newrange.render(rangeForm));
     }
 
-    public Result status(){
-	return ok(status.render());
+    public Result submitNewRange(){
+	//TODO: get data and store it.
+	return ok();
     }
 
-    public Result submitNewRange(){
-	return ok();
+    public Result ranges(){
+	//TODO: fetch data from database and send it back
+	return ok(index.render("TODO GET RANGES"));
+    }
+
+    public Result status(){
+	//TODO: add API for send data back (AJAX)
+	return ok(status.render());
     }
 }
