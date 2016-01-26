eb4c166e1eb5a1c5b304946e8afdac8d80308a6a
Enrico Benini <enrico.benini5@studio.unibo.it>
2016-01-26 16:05:24 +0100

    add the Receivers implementation

--- Server/app/controllers/dataInput/NewRangeDataReceiver.scala
+++ Server/app/controllers/dataInput/NewRangeDataReceiver.scala
@@ -1,12 +1,14 @@
 package controllers.dataInput
 
 import interfaces.dataInput.IDataReceiver
-import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.{Concurrent, Iteratee, Enumerator}
 import play.api.libs.json.JsValue
 
 /**
   * Created by Enrico Benini (AKA Benkio) benkio89@gmail.com on 1/17/16.
   */
 class NewRangeDataReceiver extends IDataReceiver{
-  override def getStream(data: JsValue): Enumerator[JsValue] = ??? //TODO
+  override def getStream(data: JsValue): Enumerator[JsValue] = {
+    buildEnumerator(data)
+  }
 }
--- Server/app/controllers/dataInput/RequestDataReceiver.scala
+++ Server/app/controllers/dataInput/RequestDataReceiver.scala
@@ -8,5 +8,7 @@ import play.api.libs.json.JsValue
   * Created by Enrico Benini (AKA Benkio) benkio89@gmail.com on 1/17/16.
   */
 class RequestDataReceiver extends IDataReceiver{
-  override def getStream(data: JsValue): Enumerator[JsValue] = ??? //TODO
+  override def getStream(data: JsValue): Enumerator[JsValue] = {
+    buildEnumerator(data)
+  }
 }
