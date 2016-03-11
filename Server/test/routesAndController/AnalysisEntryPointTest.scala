796f287713f7407158fd661b96617b25aa54fb5e
benkio89 <benkio89@gmail.com>
2016-03-11 14:32:20 +0100

    try to start the route testing

---
+++ Server/test/routesAndController/AnalysisEntryPointTest.scala
@@ -0,0 +1,26 @@
+package routesAndController
+
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+  * Created by root on 3/11/16.
+  */
+@RunWith(classOf[JUnitRunner])
+class AnalysisEntryPointTest extends Specification {
+
+
+  "the AnalysisEntryPoint" should {
+    "where the route must be /DomoticRoomServer/Analysis with 200" in new WithApplication {
+        val result = route(app, FakeRequest(GET, "/domoticRoom/analysis")).get
+
+        status(result) must equalTo(OK)
+        contentType(result) must beSome.which(_ == "text/html")
+
+    }
+  }
+}
