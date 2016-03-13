652ef3aefbd06add355f4c2ca87752f61dcf1d66
benkio <benkio89@gmail.com>
2016-03-13 12:02:03 +0100

    Add an echo end point to test the fsharp script Add the test for the echo endpoint. Test the script and the response by the play app
    All works

--- tools/ServerApiTester/ServerApiTester.fsx
+++ tools/ServerApiTester/ServerApiTester.fsx
@@ -33,23 +33,29 @@
 
 #r "bin/FSharp.Data.dll"
 open FSharp.Data
+//open FSharp.Data.HttpContentTypes
+open FSharp.Data.HttpRequestHeaders
 open System
 open System.Threading
 
-printfn "Welcom to the Server Api Tester"
+printfn "Welcome to the Server Api Tester"
 printfn "==============================="
 
+// Some parameters
+let targetApiUrl = "http://localhost:9000/echo"
 let sleepTimeMillis = 5000
 let recursionTimes = 10
 
+// Translate random int 0->100 to boolean
 let booleanFromRandom rnd = if (rnd <= 50) then true else false
 
-let jsonBooleanToSend (sensorName: string) (sensorType: int) (value: bool) (date: string)  = sprintf "{ \sensorName\: %s, \"sensorType\": %d, \"value\": %b, \"date\": %s } " sensorName sensorType value date
+// Function to build the First of the two json to randomly send.
+let jsonBooleanToSend (sensorName: string) (sensorType: int) (value: bool) (date: string)  = sprintf "{ \"sensorName\": \"%s\", \"sensorType\": %d, \"value\": %b, \"date\": \"%s\" } " sensorName sensorType value date
 
-let jsonToSend (sensorName: string) (sensorType: int) (value: int) (date: string)  = sprintf "{ \sensorName\: %s, \"sensorType\": %d, \"value\": %d, \"date\": %s } " sensorName sensorType value date
+// Function to build the Second of the two json to randomly send.
+let jsonToSend (sensorName: string) (sensorType: int) (value: int) (date: string)  = sprintf "{ \"sensorName\": \"%s\", \"sensorType\": %d, \"value\": %d, \"date\": \"%s\" } " sensorName sensorType value date
 
-
-
+// Function that randomly choose one of the previous json.
 let randomBuildBodyRequest () =
     let rnd = new Random()
     match rnd.Next(100) with
@@ -60,13 +66,19 @@ let randomBuildBodyRequest () =
             printfn "Send Non-Boolean Sensor Json"
             jsonToSend "nonBooleanSensorName" 5 (rnd.Next(100)) (DateTime.UtcNow.ToString "yyyy-MM-ddTHH:mm:ss.fffZ")
 
+// Main function.
 let rec send recursionTime =
     match recursionTime with
         | 0 -> "Program Finished"
         | _ ->  printfn "Sleep for %d milliseconds" sleepTimeMillis
                 Thread.Sleep(sleepTimeMillis)
                 let bodyRequest = randomBuildBodyRequest()
-                printfn "%s" bodyRequest
+                printfn "Body Request: %s" bodyRequest
+                let bodyResponse = Http.RequestString
+                                       ( targetApiUrl,
+                                         headers = [ ContentType HttpContentTypes.Json ],
+                                         body = TextRequest bodyRequest)
+                printfn "Body Response: %s" bodyResponse
                 send (recursionTime-1)
 
 send recursionTimes
